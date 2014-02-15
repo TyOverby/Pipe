e222746f92b13e051adb44cac1758a349acb45d3
Meyer Kizner <meyer@pre-alpha.com>
2014-02-15 08:09:15 -0500

    Rearrange the parser order to make precedence more obvious

--- src/main/scala/com/prealpha/pipe/MathParser.scala
+++ src/main/scala/com/prealpha/pipe/MathParser.scala
@@ -20,22 +20,6 @@ object MathParser extends RegexParsers {
 
   def expr: Parser[String] = symbol | macro_ | divideExpr
 
-  def basicExpr: Parser[String] = (parenExpr ^^ { "(" + _ + ")" }) | normalExpr
-
-  def spacedExpr: Parser[String] = whiteSpace.? ~> expr <~ whiteSpace.?
-
-  def exprList: Parser[List[String]] = spacedExpr ~ ("," ~> spacedExpr).* ^^ {
-    case head ~ tail => head :: tail
-  }
-
-  def twoExprList: Parser[String ~ String] = (spacedExpr <~ ",") ~ spacedExpr
-  
-  def normalExpr: Parser[String] = "[^\\s/():!,;]+".r
-  
-  def parenExpr: Parser[String] = "(" ~> spacedExpr.+ <~ ")" ^^ {
-    case list => ("" /: list)(_ + _)
-  }
-
   def divideExpr: Parser[String] = horizontalDivide | verticalDivide | basicExpr
 
   def slash: Parser[String] = whiteSpace.? ~> "/" <~ whiteSpace.?
@@ -49,6 +33,22 @@ object MathParser extends RegexParsers {
   def verticalDivide: Parser[String] = whiteSpace.? ~> basicExpr ~ slash ~ basicExpr <~ whiteSpace.? ^^ {
     case leftExpr ~ slash ~ rightExpr => leftExpr + slash + rightExpr
   }
+
+  def basicExpr: Parser[String] = (parenExpr ^^ { "(" + _ + ")" }) | normalExpr
+
+  def parenExpr: Parser[String] = "(" ~> spacedExpr.+ <~ ")" ^^ {
+    case list => ("" /: list)(_ + _)
+  }
+
+  def normalExpr: Parser[String] = "[^\\s/():!,;]+".r
+
+  def spacedExpr: Parser[String] = whiteSpace.? ~> expr <~ whiteSpace.?
+
+  def exprList: Parser[List[String]] = spacedExpr ~ ("," ~> spacedExpr).* ^^ {
+    case head ~ tail => head :: tail
+  }
+
+  def twoExprList: Parser[String ~ String] = (spacedExpr <~ ",") ~ spacedExpr
 
   def symbol: Parser[String] = ":" ~> (not(whiteSpace) ~> ".".r).* ^^ {
     case chars => "\\" + ("" /: chars)(_ + _)
