555bc0ff1d8c23a6ec9fdcf342c4c103a99d11d2
Meyer Kizner <meyer@pre-alpha.com>
2014-02-16 06:08:56 -0500

    Make tables work better

--- src/main/scala/com/prealpha/pipe/generators/latex/TableBlock.scala
+++ src/main/scala/com/prealpha/pipe/generators/latex/TableBlock.scala
@@ -1,6 +1,6 @@
 package com.prealpha.pipe.generators.latex
 
-import com.prealpha.pipe.Block
+import com.prealpha.pipe.{InlineParser, Block}
 import com.prealpha.pipe.generators._
 
 class TableBlock extends BlockGenerator {
@@ -13,10 +13,12 @@ class TableBlock extends BlockGenerator {
     val alignLine = block.childLines.head
     val contentLines = block.childLines.tail.filter(!_.trim.isEmpty)
 
-    val separators = getColumnSeparators(contentLines.head)
-    for (line <- contentLines) {
-      if (!line.trim.matches("-+") && getColumnSeparators(line) != separators)
-        throw ColumnAlignmentException(line)
+    val separators = getColumnSeparators(alignLine)
+    contentLines.foreach {
+      case line =>
+        val lineSeparators = getColumnSeparators(line)
+        if (!(Set(separators:_*) subsetOf Set(lineSeparators:_*)))
+          throw new ColumnAlignmentException(line)
     }
 
     val alignments = getColumnAlignment(alignLine, separators.unzip._1)
@@ -25,13 +27,26 @@ class TableBlock extends BlockGenerator {
     }).mkString + separators.last._2
     sb.append(s"\\begin{tabularx}{\\linewidth}{$alignStr}\n")
     if (alignLine.contains("-"))
-      sb.append("  \\hline\n")
+      sb.append("\\hline\n")
 
     for (line <- contentLines) {
-      if (line.trim.matches("[- :|]+") && line.contains("-"))
-        sb.append("  \\hline\n")
-      else
-        sb.append("  " + line.split(":|\\|").map(_.trim).filter(!_.isEmpty).mkString(" & ") + " \\\\\n")
+      if (line.trim.matches("[- :|]+") && line.contains("-")) {
+        sb.append("\\hline\n")
+      } else {
+        val lb = new StringBuilder
+        line.zipWithIndex.foreach {
+          case (c, i) =>
+            if (i == separators.head._1)
+              lb.append(" ")
+            else if (separators.tail.unzip._1.contains(i))
+              lb.append("&")
+            else
+              lb.append(c)
+        }
+        val rawStr = lb.toString().trim.dropRight(1)
+        val parsedStr = rawStr.split("&").map(str => InlineParser(str).get).mkString("&")
+        sb.append(parsedStr + " \\\\\n")
+      }
     }
 
     sb.append("\\end{tabularx}")
@@ -46,7 +61,7 @@ class TableBlock extends BlockGenerator {
 
   private def getColumnAlignment(alignLine: String, separators: List[Int]): List[ColumnAlignment] = separators match {
     case start :: end :: tail =>
-      val alignStr = alignLine.substring(start + 1, end).trim
+      val alignStr = alignLine.substring(start + 2, end - 1)
       val ourAlign =
         if (alignStr.length > 2 && alignStr.matches("\\++"))
           FillAlign
--- src/test/scala/com/prealpha/pipe/generators/latex/TableBlockTest.scala
+++ src/test/scala/com/prealpha/pipe/generators/latex/TableBlockTest.scala
@@ -28,13 +28,34 @@ class TableBlockTest extends FlatSpec with Matchers {
       be (
         """
           |\begin{tabularx}{\linewidth}{lcr}
-          |  \hline
-          |  Tables & Are & Cool \\
-          |  \hline
-          |  col 3 is & right-aligned & $1600 \\
-          |  col 2 is & centered & $12 \\
-          |  zebra stripes & are neat & $1 \\
+          |\hline
+          |Tables        & Are           & Cool   \\
+          |\hline
+          |col 3 is      & right-aligned & $1600  \\
+          |col 2 is      & centered      &   $12  \\
+          |zebra stripes & are neat      &    $1  \\
           |\end{tabularx}
         """.stripMargin.trim)
+  }
+
+  it should "parse this" in {
+    val markup =
+      """
+        |(|$ A |) can be transformed to (|$ B |) by subtracting two times the first column from the second.
+        |
+        |(|$ B |) can be transformed to (|$ C |) by subtracting the first row from the second.
+        |
+        |The following sequence of elementary (row) operations transforms (|$ C |) into (|$ I_3 |):
+        |
+        ||table
+        |  : +                                             : ++++++++++++++++++++++++++++++++++++++++++++ :
+        |  : (|$ !matrix(1, 0, 3; 0, -2, -2; 0, -3, -2) |) : Subtract the first row from the third.       :
+        |  : (|$ !matrix(1, 0, -3; 0, 1, 1; 0, -3, -2) |)  : Multiply the second row by (|$ -1/2 |).      :
+        |  : (|$ !matrix(1, 0, -3; 0, 1, 1; 0, 0, 1) |)    : Add three times the second row to the third. :
+        |  : (|$ !matrix(1, 0, -3; 0, 1, 0; 0, 0, 1) |)    : Subtract the third row from the second.      :
+        |  : (|$ !matrix(1, 0, 0; 0, 1, 0; 0, 0, 1) |)     : Add three times the third row to the first.  :
+        |
+      """.stripMargin.trim
+    println(compile(parse(markup)))
   }
 }
