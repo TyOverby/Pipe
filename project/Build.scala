8e0988e4579ffc07f6d1dbfda5f01ea5a1ff948a
TyOverby <ty@pre-alpha.com>
2014-03-09 05:16:49 -0700

    Project probably (maybe) builds with scala.js

--- project/Build.scala
+++ project/Build.scala
@@ -8,34 +8,37 @@ import ScalaJSPlugin._
 object Build extends sbt.Build {
 
     val commonSettings = Defaults.defaultSettings ++ Seq(
-        scalaVersion := "2.10.3",
+        scalaVersion := "2.11.0-M8",
         version := "0.1.0"
     )
 
     lazy val root = Project(
         id = "vulcan",
         base = file("."),
-        settings = commonSettings ++ packSettings ++
+        settings = scalaJSSettings ++ commonSettings ++ packSettings ++
         Seq(
-            packMain := Map("vulcan" -> "com.prealpha.pipe.blocks.generators.latex.LatexDocument")
+           packMain := Map("vulcan" -> "com.prealpha.pipe.blocks.generators.latex.LatexDocument")
         )
+
     ).dependsOn(math, blocks).aggregate(math, blocks)
 
     lazy val math = Project(
         id = "math",
         base = file("math"),
-        settings = commonSettings ++ scalaJSSettings ++ Seq(
+        settings = scalaJSSettings ++ commonSettings ++ Seq(
             libraryDependencies += "org.xerial" % "xerial-core" % "3.2.1",
-            libraryDependencies += "org.scalatest" % "scalatest_2.10" % "latest.integration" % "test"
+            libraryDependencies += "org.scalatest" % "scalatest" % "latest.integration" % "test",
+            libraryDependencies += "org.scalajs" %% "scalajs-parser-combinators" % "1.0.0-SNAPSHOT"
         )
     )
 
     lazy val blocks = Project(
         id = "blocks",
         base = file("blocks"),
-        settings = commonSettings ++ scalaJSSettings ++ Seq(
+        settings = scalaJSSettings ++ commonSettings ++ Seq(
             libraryDependencies += "org.xerial" % "xerial-core" % "3.2.1",
-            libraryDependencies += "org.scalatest" % "scalatest_2.10" % "latest.integration" % "test"
+            libraryDependencies += "org.scalatest" % "scalatest" % "latest.integration" % "test",
+            libraryDependencies += "org.scalajs" %% "scalajs-parser-combinators" % "1.0.0-SNAPSHOT"
         )
     ) dependsOn(math)
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,3 +1,5 @@
+resolvers += Resolver.url("scala-js-snapshots", url("http://repo.scala-js.org/repo/snapshots/"))(Resolver.ivyStylePatterns)
+
 addSbtPlugin("org.xerial.sbt" % "sbt-pack" % "0.5.0")
 
-addSbtPlugin("org.scala-lang.modules.scalajs" % "scalajs-sbt-plugin" % "0.3")
+addSbtPlugin("org.scala-lang.modules.scalajs" % "scalajs-sbt-plugin" % "0.4-SNAPSHOT")
