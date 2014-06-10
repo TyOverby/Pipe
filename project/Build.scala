98051c143caa7f85b2ae71d96b5a1543086fe01b
TyOverby <ty@pre-alpha.com>
2014-06-10 09:47:07 -0700

    Tests run again
    For some reason, adding scalaJSSettings to a project makes it not
    run the tests that it should.  Scala-js is disabled until this is
    fixed.

--- project/Build.scala
+++ project/Build.scala
@@ -11,34 +11,31 @@ object Build extends sbt.Build {
         version := "0.4.2"
     )
 
-    //lazy val scalajs_parser = RootProject(uri("https://github.com/sjrd/scala-parser-combinators.git"))
-
     lazy val root = Project(
         id = "vulcan",
         base = file("."),
-        settings = scalaJSSettings ++ commonSettings ++ packSettings ++
+        settings = commonSettings ++ packSettings ++
         Seq(
             packMain := Map("vulcan" -> "com.prealpha.pipe.document.EntryPoint")
         )
-    ).dependsOn(math, document/*, scalajs_parser*/).aggregate(math, document)
+    ).dependsOn(math, document).aggregate(math, document)
 
 
     lazy val math = Project(
         id = "math",
         base = file("math"),
-        settings = scalaJSSettings ++ commonSettings ++ Seq(
+        settings = commonSettings ++ Seq(
             libraryDependencies += "org.scala-lang.modules" %% "scala-parser-combinators" % "1.0.1",
             libraryDependencies += "org.scalatest" % "scalatest_2.11" % "latest.integration" % "test",
-            libraryDependencies += "org.xerial" % "xerial-core" % "3.2.1",
-            libraryDependencies += "org.scalajs" %%% "scala-parser-combinators" % "1.0.2-SNAPSHOT"
-            //libraryDependencies += scalajs_parser
+            libraryDependencies += "org.xerial" % "xerial-core" % "3.2.1"
+            //libraryDependencies += "org.scalajs" %%% "scala-parser-combinators" % "1.0.2-SNAPSHOT"
         )
-    )//.dependsOn(scalajs_parser)
+    )
 
     lazy val document = Project(
         id = "document",
         base = file("document"),
-        settings = scalaJSSettings ++ commonSettings ++ Seq(
+        settings = commonSettings ++ Seq(
             libraryDependencies += "org.scala-lang.modules" %% "scala-parser-combinators" % "1.0.1",
             libraryDependencies += "org.scalatest" % "scalatest_2.11" % "latest.integration" % "test",
             libraryDependencies += "org.xerial" % "xerial-core" % "3.2.1"
